220c91922bcf5a255bc1516467226afdd63f4b85
John South <im.john.south@gmail.com>
2020-01-25 12:17:29 -0800

    added basic renderSelector method to app.jsx

---
+++ client/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      currentView: true
+    }
+  }
+
+  renderSelector(){
+    let components;
+
+    return components;
+  }
+
+  render(){
+    return(
+      <div>PigSmart</div>
+    );
+  }
+}
