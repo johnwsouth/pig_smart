2e3620fbafaeb301a4e08c7b76af62a35c7096b3
John South <im.john.south@gmail.com>
2020-01-25 14:02:09 -0800

    seperate widths for nav items based on text sizing

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import NavBar from './containers/nav-bar'
+import NavBar from './components/nav-bar';
+import LandingPage from './containers/landing-page'
 
 export default class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      currentView: true
+      currentPage: 'landing-page',
     }
   }
 
   renderSelector(){
-    let components = <NavBar/>;
-    return components;
+    let components;
+    let returnedComponents;
+    if (this.state.currentPage === 'landing-page'){
+      returnedComponents = <LandingPage/>
+    }
+
+    return (
+      <>
+      <NavBar/>
+      {returnedComponents}
+      </>
+      );
   }
 
   render(){
--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -4,15 +4,16 @@ export default class NavBar extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      currentPage: '',
+      currentPage: 'landing-page',
     }
   }
 
   render(){
     let navItemTextArray = ['macronutrient calculator', 'recipes', 'special offers']
+    let navItemClassArray = ['macronutrient-nav', 'recipes-nav', 'special-offers-nav']
     let navItemArray = []
     for(let i = 0; i < 3; i++){
-      navItemArray.push(<div className='nav-bar-item'>{navItemTextArray[i]}</div>)
+      navItemArray.push(<div key={navItemTextArray[i]} className={'nav-bar-item ' + navItemClassArray[i]} >{navItemTextArray[i]}</div>)
     }
     return(
     <div className="nav-bar-container">
